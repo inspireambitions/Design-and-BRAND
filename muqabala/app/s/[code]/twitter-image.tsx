5c6121757c6f4c54800713324a25eb582e6c4be4
inspireambitions <156596849+inspireambitions@users.noreply.github.com>
2026-08-28 17:45:01 +0400

    Resolve production social image changes

--- muqabala/app/s/[code]/twitter-image.tsx
+++ muqabala/app/s/[code]/twitter-image.tsx
@@ -1,2 +1,8 @@
-export { alt, contentType, default, size } from './opengraph-image';
+import ScreeningOpenGraphImage from './opengraph-image';
+
+export const alt = 'A Muqabala work-sample invitation from a hiring team';
+export const size = { width: 1200, height: 630 };
+export const contentType = 'image/png';
 export const dynamic = 'force-dynamic';
+
+export default ScreeningOpenGraphImage;
