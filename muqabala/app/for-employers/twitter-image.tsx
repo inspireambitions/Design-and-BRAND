0835b81e61e8e459d006b9a249e835147fe66bcd
Inspire Ambitions <inspireambition.com@gmail.com>
2026-08-28 16:33:10 +0400

    Fix Vercel social image routes

--- muqabala/app/for-employers/twitter-image.tsx
+++ muqabala/app/for-employers/twitter-image.tsx
@@ -1,1 +1,7 @@
-export { alt, contentType, default, size } from './opengraph-image';
+import EmployerOpenGraphImage from './opengraph-image';
+
+export const alt = 'Muqabala work samples for hiring teams';
+export const size = { width: 1200, height: 630 };
+export const contentType = 'image/png';
+
+export default EmployerOpenGraphImage;
--- muqabala/app/s/[code]/twitter-image.tsx
+++ muqabala/app/s/[code]/twitter-image.tsx
@@ -1,1 +1,8 @@
-export { alt, contentType, default, dynamic, size } from './opengraph-image';
+import ScreeningOpenGraphImage from './opengraph-image';
+
+export const alt = 'A Muqabala work-sample invitation from a hiring team';
+export const size = { width: 1200, height: 630 };
+export const contentType = 'image/png';
+export const dynamic = 'force-dynamic';
+
+export default ScreeningOpenGraphImage;
